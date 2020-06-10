cdc1954a290a17ee18547d22b3e789a3ce6d5827
kyle-test-68 <60332664+kyle-test-68@users.noreply.github.com>
2020-06-10 17:00:53 +0800

    ..

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-console.log("Service Worker is here.");
+console.log('Start of service worker');
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/offline.html'
+];
+
+
+self.addEventListener('install', event => {
+    console.log("Service Worker installed");
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
